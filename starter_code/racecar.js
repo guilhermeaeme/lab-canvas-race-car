ead4e02465a748e89585c0b62f29b899ef89bc22
guilhermeaeme <guilhermeaeme@gmail.com>
2018-12-07 21:36:23 -0200

    drawRoad

--- starter_code/racecar.js
+++ starter_code/racecar.js
@@ -3,16 +3,28 @@ function RaceCar() {
 	this.ctx = this.canvas.getContext('2d'),
 
 	this.startGame = function() {
+		this.drawRoad();
+	},
+
+	this.drawRoad = function() {
 		this.ctx.fillStyle = "green";
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 		this.ctx.fillStyle = "#808080";
 		this.ctx.fillRect(30, 0, this.canvas.width-60, this.canvas.height);
 
-		var middle = this.canvas.width/2 - 3;
-
-		this.ctx.beginPath();
 		this.ctx.lineWidth = 6;
 		this.ctx.strokeStyle = "#FFFFFF";
+
+
+		this.ctx.beginPath();
+
+		this.ctx.moveTo(40, 0);
+		this.ctx.lineTo(40, this.canvas.height);
+
+		this.ctx.moveTo(this.canvas.width - 40, 0);
+		this.ctx.lineTo(this.canvas.width - 40, this.canvas.height);
+
+		var middle = this.canvas.width/2 - 3;
 
 		for(i=15; i<this.canvas.height; i+=60) {
 			this.ctx.moveTo(middle, i);
